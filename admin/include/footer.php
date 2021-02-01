4ae4f2e93df76967e57e2ac07203aff2a3b39a38
NancyTendo <61803693+NancyTendo@users.noreply.github.com>
2021-02-01 10:15:30 -0600

    Correcion de Errores

--- admin/include/footer.php
+++
@@ -1,7 +0,0 @@
-	<div class="footer">
-		<div class="container">
-			 
-
-			<b class="copyright">&copy; SuperM.com</b> 
-		</div>
-	</div>
--- admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION['alogin']=="";
-session_unset();
-$_SESSION['errmsg']="Inicie Secion para administrar la pagina";
-?>
-<script language="javascript">
-document.location="index.php";
-</script>
