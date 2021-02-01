beb4b098d9df2179fe5d92257c40352666718cd2
Yaretzi <yareperezms@gmail.com>
2021-02-01 01:04:29 -0600

    Revision de Progreso

--- includes/myaccount-sidebar.php
+++ includes/myaccount-sidebar.php
@@ -1,17 +1,17 @@
-	<div class="col-md-4">
+<div class="col-md-4">
 					<!-- checkout-progress-sidebar -->
 <div class="checkout-progress-sidebar ">
 	<div class="panel-group">
 		<div class="panel panel-default">
 			<div class="panel-heading">
-		    	<h4 class="unicase-checkout-title">Your Checkout Progress</h4>
+		    	<h4 class="unicase-checkout-title">Revisar progreso</h4>
 		    </div>
 		    <div class="panel-body">
 				<ul class="nav nav-checkout-progress list-unstyled">
-					<li><a href="my-account.php">My Account</a></li>
-					<li><a href="bill-ship-addresses.php">Shipping / Billing Address</a></li>
-					<li><a href="order-history.php">Order History</a></li>
-					<li><a href="pending-orders.php">Payment Pending Order</a></li>
+					<li><a href="my-account.php">Mi cuenta</a></li>
+					<li><a href="bill-ship-addresses.php">SuperM.com/ </a></li>
+					<li><a href="order-history.php">Historial</a></li>
+					<li><a href="pending-orders.php">Orden pendiente de pago</a></li>
 				</ul>		
 			</div>
 		</div>
